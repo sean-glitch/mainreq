0c0769e232654fd18365e05018167740a0f1c05d
Glitch (mainrequest) <none>
2021-02-08 12:14:25 +0000

    🌹🏚 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,2 +1,138 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
+$(document).ready(function() {
+  let testMessage = () => {
+    console.log("dataSent");
+  };
+
+  var reqCards = [];
+
+  function passWord() {
+    var testV = 1;
+    var pass1 = prompt("Please Enter Your Password", " ");
+    while (testV < 3) {
+      if (!pass1) history.go(-1);
+      if (pass1.toLowerCase() == 'fogcreek') {
+        var notes = prompt("You Got it Right! Please enter order information here", " ");
+        var cardID = { id: event.srcElement.id,
+                       notes: notes
+                     };
+        $.post("/ordered", cardID, async data => {
+      console.log("Sent order info to database.");
+    });
+        break;
+      }
+      testV += 1;
+      var pass1 = prompt(
+        "Access Denied - Password Incorrect, Please Try Again.",
+        "Password"
+      );
+    }
+    if ((pass1.toLowerCase() == 'fogcreek') & (testV == 3)) history.go(-1);
+    return " ";
+  }
+
+  function updateNote(){
+    var noteID = event.srcElement.id;
+    noteID = noteID.split("-", 1);
+    var addNotes = prompt("Please enter your note here", " ");
+    $.post("/updatenote", { id: noteID, note: addNotes }, async data => {
+      console.log("Sent note to database.")
+    });
+  }
+  
+  function firstNote(){
+    alert('Update coming very soon');
+  }
+  
+  $.get("/getcards", "JSON", async data => {
+    try {
+      var dataObj = await JSON.parse(data);
+      reqCards = await Object.values(dataObj);
+
+      for (var i = 0; i < reqCards.length; i++) {
+        var drc = new Date(reqCards[i].created);
+        drc = drc.toJSON().substring(0, 10);
+        var notesField;
+        if(reqCards[i].notes == null){notesField = "none";} else {notesField = reqCards[i].notes;}
+        $("#tracking-area")
+          .append(`<div class="right__tracking-area--card" id="card${reqCards[i].id}"><div class="right__tracker-area--card--list right__tracker-area--card--list--h2"><h2>${reqCards[i].employee}</h2></div>
+    <div class="right__tracker-area--card--list">Item: ${reqCards[i].item}</div>
+    <div class="right__tracker-area--card--list">Category: ${reqCards[i].category}</div>
+    <div class="right__tracker-area--card--list">Requested On: ${drc}</div>
+    <div class="right__tracker-area--card--list">Status: ${reqCards[i].status}</div>
+    <div class="right__tracker-area--card--list" id="notes${reqCards[i].id}">Notes: ${notesField}</div>
+    <div><button class="ordered" id='${reqCards[i].id}'>Order</button></div>
+    <div><button class="addnote" id='${reqCards[i].id}-note'>Add Note</button></div>
+    <div><button class="complete" id='${reqCards[i].id}-complete'>Complete</button></div>
+    </div>`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  $("#form-r").submit(function() {
+    // Variables for form input values
+    var empName = $("#employee_name").val();
+    var item = $("#item").val();
+    var linkAddress = $("#link_address").val();
+    var whenNeeded = $("#when_needed").val();
+    var whyNeeded = $("#why_needed").val();
+    var category = $("#category").val();
+    var reqTime = Date.now();
+
+    // Build object to send over to server
+    let initialRequest = {
+      employee: empName,
+      item: item,
+      link: linkAddress,
+      when_needed: whenNeeded,
+      why_needed: whyNeeded,
+      category: category,
+      request_time: reqTime
+    };
+
+    //let sendRequest = JSON.stringify(initialRequest);
+
+    // Send info to server to be put into database
+    $.post("/newrequest", initialRequest, async data => {
+      try {
+        var dataObjR = await JSON.parse(data);
+        reqCards = await Object.values(dataObjR);
+
+        for (var i = 0; i < reqCards.length; i++) {
+          var drc = new Date(reqCards[i].created);
+          drc = drc.toJSON().substring(0, 10);
+          $("#tracking-area")
+            .append(`<div class="right__tracking-area--card"><h2>Name: ${reqCards[i].employee}</h2>
+    Item: ${reqCards[i].item}<br><br>
+    Category: ${reqCards[i].category}<br><br>
+    Requested On: ${drc}<br><br>
+    Status: ${reqCards[i].status}<br><br>
+    </div>`);
+          alert("You're Request Has Been Added!");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    return false;
+  });
+
+  $("body").delegate(".ordered", "click", function() {
+    passWord();
+  });
+  
+  $("body").delegate(".addnote", "click", function() {
+    var ttt = event.srcElement.id;
+    ttt = ttt.split("-", 1);
+    var sss = 'notes' + ttt;
+    var bbb = $("#"+sss).text();
+    if(bbb == "Notes: none"){
+      firstNote();
+    } else {
+      updateNote();
+    }
+  });
+});
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
+const nodemailer = require("nodemailer");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+var recID;
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -22,7 +24,7 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
 
 
-
+/*
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(() => {
     console.log('Databases ready to go!');
@@ -33,19 +35,101 @@ db.serialize(() => {
     });
   }
 );
+*/
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
 });
 
+/*
 // endpoint to get all the dreams in the database
 app.get("/getDreams", (request, response) => {
   db.all("SELECT * from Dreams", (err, rows) => {
-    response.send(JSON.stringify(rows));
+    response.send(rows);
+  });
+});
+*/
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMU,
+    pass: process.env.GMP
+  }
+});
+
+app.get("/getCards", async (request, response) => {
+  await db.all("SELECT * from Requests", (err, rows) => {
+    var sending = JSON.stringify(rows);
+    response.send(sending);
+  });
+})
+
+app.post("/newrequest", async (request, response) => {
+  //let sentRequest = JSON.parse(request);
+  await db.run(`INSERT INTO Requests(employee, item, link, why_needed, when_needed, category, status, created) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`, [request.body.employee, request.body.item, request.body.link, request.body.why_needed, request.body.when_needed, request.body.category, "New", request.body.request_time], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+    recID = this.lastID;
+    console.log(`this is ${recID}`);
+    db.all(`SELECT * from Requests WHERE id='${recID}'`, (err, rows) => {
+    var sending = JSON.stringify(rows);
+    response.send(sending);
+  });
+    var mailOptions = {
+  from: 'thesportstrifecta@gmail.com',
+  to: 'sean@glitch.com',
+  subject: 'New Glitch Request',
+  text: `${request.body.employee} has requested ${request.body.item} for ${request.body.when_needed}. The reason is ${request.body.why_needed} Use this link please: ${request.body.link}`
+};
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent' + info.response);
+      }
+    });
+  });
+  
+});
+
+app.post("/ordered", async (request, response) => {
+  let reqID = request.body.id;
+  let notes = request.body.notes;
+  console.log(notes);
+  await db.run(`UPDATE Requests SET status = 'Ordered', notes = '${notes}' WHERE id = ${reqID}`, function(err){
+    if (err) {
+      return console.log(err.message);
+    }
+    console.log(`Updated ${reqID} to ordered`);
+    
   });
 });
 
+app.post("/updatenote", async (request, response) => {
+  let noteID = request.body.id;
+  let notes = request.body.note;
+  console.log(notes);
+  await db.run(`UPDATE Requests SET notes = notes || '<br><br>${notes}' WHERE id = ${noteID}`, function(err){
+    if (err) {
+      return console.log(err.message);
+    }
+    console.log(`Updated ${noteID}'s notes`);
+  });
+  
+ /* var respData = await db.run(`SELECT notes FROM Requests WHERE id = ${noteID}`, function(err, rows){
+    if (err) {
+      return console.log(err.message);
+    }
+ });
+  console.log('the data is ' + respData); */
+});
+
+/*
 // endpoint to add a dream to the database
 app.post("/addDream", (request, response) => {
   console.log(`add to dreams ${request.body.dream}`);
@@ -88,6 +172,7 @@ app.get("/clearDreams", (request, response) => {
     );
   }
 });
+*/
 
 // helper function that prevents html/css/script malice
 const cleanseString = function(string) {
